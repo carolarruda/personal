4fbfc1f5c4469da23debc511d2308801cc551142
carolina arruda <carolinacalouroarruda@gmail.com>
2023-11-02 12:45:44 +0100

    added resolution library

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,10 +4,19 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import NavBarBigResol from "./NavBarBigResol copy";
 import NavBarPhone from "./NavBarPhone";
+import { useMediaQuery } from 'react-responsive'
 
 const NavBar = () => {
   const [selected, setSelected] = useState("");
   const [isMobile, setIsMobile] = useState(false);
+
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  })
+  const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
 
   const handleResize = () => {
     if (window.innerWidth < 720) {
@@ -45,14 +54,14 @@ const NavBar = () => {
   return (
     <header className={classes.NavBar}>
       <motion.nav
-        className={`${classes.NavContainer} ${classes.mxAuto} ${classes.navOnTop}`}
+        className={isDesktopOrLaptop ? `${classes.NavContainer} ${classes.mxAuto} ${classes.navOnTop}` : `${classes.NavContainer} ${classes.navOnTop}` }
         initial={{ y: -70 }}
         animate={{ y: 0 }}
         transition={{ duration: 0.5 }}
       >
         <HomeLogo />
-        {!isMobile && <NavBarBigResol />}
-        {isMobile && <NavBarPhone />}
+        {isDesktopOrLaptop  && <NavBarBigResol />}
+        {isTabletOrMobile  && <NavBarPhone />}
       </motion.nav>
     </header>
   );
