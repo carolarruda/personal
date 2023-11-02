63d2ce936d286741ac9896f302316ad407bdcce5
carolina arruda <carolinacalouroarruda@gmail.com>
2023-11-02 10:20:49 +0100

    testing

--- src/components/blobs/NavBlob.js
+++ src/components/blobs/NavBlob.js
@@ -9,7 +9,7 @@ const NavBlob = () => {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1200 120"
         preserveAspectRatio="none"
-        style={{ borderTop: "1px solid #dab7a5" }}
+        style={{ borderTop: "5px solid blue" }}
       >
         <path
           d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z"
