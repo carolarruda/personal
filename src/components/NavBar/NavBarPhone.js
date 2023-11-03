79b68918e6f8f33076b9637b0e8207ea6b1eda17
carolina arruda <carolinacalouroarruda@gmail.com>
2023-11-03 08:00:06 +0100

    fixed button logic

--- src/components/NavBar/NavBarPhone.js
+++ src/components/NavBar/NavBarPhone.js
@@ -3,59 +3,66 @@ import classes from "./NavBarPhone.module.scss";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const NavBarPhone = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  let [isOpen, setisOpen] = useState(false);
 
   const handleMenu = () => {
-    setMenuOpen((prevState) => !prevState);
+    setisOpen((prevState) => !prevState);
   };
 
   const handleItemClick = () => {
-    setMenuOpen(false);
+    setisOpen(false);
   };
 
   return (
     <>
-      <button onClick={handleMenu} className={classes.iconContainer}>
-        {menuOpen ? (
+      <button
+        
+        type="button"
+        onClick={handleMenu}
+        className={classes.iconContainer}
+      >
+        {isOpen ? (
           <AiOutlineClose className={classes.menuIcon} />
         ) : (
           <AiOutlineMenu className={classes.menuIcon} />
         )}
       </button>
-     
-        <div style={{display: menuOpen ? 'initial' : 'none'}}  className={classes.menuContainer}>
-          <ul className={classes.NavOptions}>
-            <li className={classes.grey}>
-              <a href="#top" onClick={handleItemClick}>
-                Home
-              </a>
-            </li>
-            <li className={classes.grey}>
-              <a href="#about" onClick={handleItemClick}>
-                About
-              </a>
-            </li>
-            <li className={classes.grey}>
-              <a href="#projects" onClick={handleItemClick}>
-                Projects
-              </a>
-            </li>
-            <li className={classes.grey}>
-              <a href="#contact" onClick={handleItemClick}>
-                Contact
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://app.enhancv.com/share/ca9fed4a/?utm_medium=growth&utm_campaign=share-resume&utm_source=dynamic"
-                onClick={handleItemClick}
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
-        </div>
- 
+
+      <div
+        style={{ display: isOpen ? "initial" : "none" }}
+        className={classes.menuContainer}
+      >
+        <ul className={classes.NavOptions}>
+          <li className={classes.grey}>
+            <a href="#top" onClick={handleItemClick}>
+              Home
+            </a>
+          </li>
+          <li className={classes.grey}>
+            <a href="#about" onClick={handleItemClick}>
+              About
+            </a>
+          </li>
+          <li className={classes.grey}>
+            <a href="#projects" onClick={handleItemClick}>
+              Projects
+            </a>
+          </li>
+          <li className={classes.grey}>
+            <a href="#contact" onClick={handleItemClick}>
+              Contact
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://app.enhancv.com/share/ca9fed4a/?utm_medium=growth&utm_campaign=share-resume&utm_source=dynamic"
+              onClick={handleItemClick}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
