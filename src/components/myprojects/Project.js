8146889392e7834dc1b7b838da3338df13c013c0
carolina arruda <carolinacalouroarruda@gmail.com>
2023-11-08 17:07:44 +0100

    removed opacity

--- src/components/myprojects/Project.js
+++ src/components/myprojects/Project.js
@@ -39,7 +39,7 @@ export const Project = ({
         ref={ref}
         variants={{
           hidden: { opacity: 0, y: 100 },
-          visible: { opacity: 1, y: 0 },
+
         }}
         initial="hidden"
         animate={controls}
