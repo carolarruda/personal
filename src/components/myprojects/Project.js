46a48c82fba363cf11249adc9add403101a304c1
carolina arruda <carolinacalouroarruda@gmail.com>
2023-11-08 17:09:44 +0100

    fixed issue

--- src/components/myprojects/Project.js
+++ src/components/myprojects/Project.js
@@ -38,8 +38,8 @@ export const Project = ({
       <motion.div
         ref={ref}
         variants={{
-          hidden: { opacity: 0, y: 100 },
-
+          hidden: { opacity: 1, y: 100 },
+          visible: { opacity: 1, y: 0 },
         }}
         initial="hidden"
         animate={controls}
