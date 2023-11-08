18ff94c3e4dc2b769023d702addef8c82ee21344
carolina arruda <carolinacalouroarruda@gmail.com>
2023-11-08 13:25:20 +0100

    testing new button

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,15 +12,33 @@ const About = () => {
     query: "(min-width: 1224px)",
   });
 
-
   return (
     <div className={classes.sec}>
       <div className={classes.blobAbout}>
         <Bloby />
       </div>
-      <section className='section-wrapper' id="about">
+      {/* <div className={classes.bloby}>
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            className={classes.shapeFill}
+          ></path>
+        </svg>
+      </div> */}
+      <section className="section-wrapper" id="about">
         <SectionTitle title={"About"} dir={"l"} />
-        <div className={isDesktopOrLaptop? `${classes.about}` : `${classes.about} ${classes.media}`}>
+        <div
+          className={
+            isDesktopOrLaptop
+              ? `${classes.about}`
+              : `${classes.about} ${classes.media}`
+          }
+        >
           <div>
             <Reveal>
               {" "}
