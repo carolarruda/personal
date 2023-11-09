21a9eb29f80f23a22b1a9da6fd589f5b795a0841
carolina arruda <carolinacalouroarruda@gmail.com>
2023-11-09 11:45:06 +0100

    testing buttons

--- src/components/HomeLogo/HomeLogo.js
+++ src/components/HomeLogo/HomeLogo.js
@@ -1,23 +1,31 @@
 import classes from "./HomeLogo.module.scss";
 import { useMediaQuery } from "react-responsive";
 
-
 const HomeLogo = () => {
   const isDesktopOrLaptop = useMediaQuery({
-    query: '(min-width: 1224px)'
-  })
+    query: "(min-width: 1224px)",
+  });
+
+  const isMobile = window.innerWidth <= 768;
 
   return (
-    <span
-      className={isDesktopOrLaptop ? `${classes.logo} ${classes.pointer}` : `${classes.phone} ${classes.pointer}`}
-      onClick={() => {
-        window.scrollTo(0, 0);
-      }}
-    >
-      C<span>.</span>
-    </span>
-
-
+    <>
+      {isMobile && (
+        <span
+        id={classes.border}
+          className={
+            isDesktopOrLaptop
+              ? `${classes.logo} ${classes.pointer}`
+              : `${classes.phone} ${classes.pointer}`
+          }
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+        >
+          C<span>.</span>
+        </span>
+      )}
+    </>
   );
 };
 
