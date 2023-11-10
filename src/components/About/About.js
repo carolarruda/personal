f5643b12748d5b37c14c011be87f4529cee98cd0
carolina arruda <carolinacalouroarruda@gmail.com>
2023-11-10 12:02:58 +0100

    working with hero blob

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,57 +6,58 @@ import MyLinks from "@components/Links/MyLinks";
 import { Reveal } from "@utils/Reveal/Reveal";
 import { useMediaQuery } from "react-responsive";
 
+
 const About = () => {
   const isDesktopOrLaptop = useMediaQuery({
     query: "(min-width: 1224px)",
   });
 
   return (
-    <div className={classes.sec}>
+    <>
 
-
-      <section className={`section-wrapper`} id="about">
-        <SectionTitle title={"About"} dir={"l"} />
-        <div
-          className={
-            isDesktopOrLaptop
-              ? `${classes.about}`
-              : `${classes.about} ${classes.media}`
-          }
-        >
-          <div>
-            <Reveal>
-              {" "}
-              <p className={classes.aboutText}>
-                Hey! I&apos;m Carolina, a full-stack developer and recent
-                Software Development Academy graduate based in Gothenburg,
-                Sweden. Eager to continuously expand my skill set and deliver
-                high quality software to users.
-              </p>
-            </Reveal>
-            <Reveal>
-              <p className={classes.aboutText}>
-                Outside of work, you&apos;ll often find me in the kitchen,
-                either exploring new recipes or going back to my Portuguese
-                roots. Yoga enthusiast on my spare time.
-              </p>
-            </Reveal>
-            <Reveal>
-              <div className={classes.links}>
-                <div className={classes.linksText}>
-                  <span>Links</span>
-                  <AiOutlineArrowRight />
-                  <MyLinks />
+      <div className={classes.sec}>
+        <section className={`section-wrapper`} id="about">
+          <SectionTitle title={"About"} dir={"l"} />
+          <div
+            className={
+              isDesktopOrLaptop
+                ? `${classes.about}`
+                : `${classes.about} ${classes.media}`
+            }
+          >
+            <div>
+              <Reveal>
+                {" "}
+                <p className={classes.aboutText}>
+                  Hey! I&apos;m Carolina, a full-stack developer and recent
+                  Software Development Academy graduate based in Gothenburg,
+                  Sweden. Eager to continuously expand my skill set and deliver
+                  high quality software to users.
+                </p>
+              </Reveal>
+              <Reveal>
+                <p className={classes.aboutText}>
+                  Outside of work, you&apos;ll often find me in the kitchen,
+                  either exploring new recipes or going back to my Portuguese
+                  roots. Yoga enthusiast on my spare time.
+                </p>
+              </Reveal>
+              <Reveal>
+                <div className={classes.links}>
+                  <div className={classes.linksText}>
+                    <span>Links</span>
+                    <AiOutlineArrowRight />
+                    <MyLinks />
+                  </div>
                 </div>
-              </div>
-            </Reveal>
-          </div>
+              </Reveal>
+            </div>
 
-          <Stats />
-        </div>
-      </section>
-      
-    </div>
+            <Stats />
+          </div>
+        </section>
+      </div>
+    </>
   );
 };
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import classes from "./Hero.module.scss";
 import ButtonNew from "../buttons/ButtonNew";
+import Blobtest from "../blobs/Blobtest";
 
 const Hero = () => {
   return (
@@ -22,7 +23,7 @@ const Hero = () => {
           <ButtonNew type={`projects`} text={`View Projects`} />
         </div>
       </section>
-      <div className={classes.bloby}>
+      <div className={classes.BC}>
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
@@ -30,12 +31,12 @@ const Hero = () => {
           preserveAspectRatio="none"
         >
           <path
-            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-            className={classes.shapeFill}
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            class="shape-fill"
+            fill="#e3d5d5"
           ></path>
         </svg>
       </div>
-
     </>
   );
 };
